4764328cf942cd5591cc74e147ffe67c94fb8180
Lewis Nixon <lewcfn@gmail.com>
2025-07-16 20:17:16 +0100

    Add highlighting to the active route

--- apps/web/app/dashboard/layout.tsx
+++ apps/web/app/dashboard/layout.tsx
@@ -13,40 +13,19 @@ import {
   NavbarItem,
   NavbarSection,
   NavbarSpacer,
-  Sidebar,
-  SidebarBody,
-  SidebarFooter,
-  SidebarHeader,
-  SidebarHeading,
-  SidebarItem,
-  SidebarLabel,
   SidebarLayout,
-  SidebarSection,
-  SidebarSpacer,
 } from "@/catalyst-components";
 
 import {
   ArrowRightStartOnRectangleIcon,
-  ChevronDownIcon,
-  ChevronUpIcon,
   Cog8ToothIcon,
   LightBulbIcon,
-  PlusIcon,
   ShieldCheckIcon,
   UserIcon,
 } from "@heroicons/react/16/solid";
-import {
-  CalendarDaysIcon,
-  Cog6ToothIcon,
-  HomeIcon,
-  InboxIcon,
-  MagnifyingGlassIcon,
-  MegaphoneIcon,
-  QuestionMarkCircleIcon,
-  SparklesIcon,
-  TicketIcon,
-} from "@heroicons/react/20/solid";
+import { InboxIcon, MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 import { Metadata } from "next";
+import { DashboardSidebar } from "./dashboard-sidebar";
 
 export const metadata: Metadata = {
   title: "Dashboard",
@@ -60,6 +39,7 @@ export default async function DashboardLayout({
 }) {
   const user = await prisma.user.findFirst({
     select: {
+      id: true,
       name: true,
       email: true,
     },
@@ -112,152 +92,7 @@ export default async function DashboardLayout({
           </NavbarSection>
         </Navbar>
       }
-      sidebar={
-        <aside>
-          <Sidebar>
-            <SidebarHeader>
-              <Dropdown>
-                <DropdownButton as={SidebarItem} className="lg:mb-2.5">
-                  <Avatar src="https://catalyst.tailwindui.com/tailwind-logo.svg" />
-                  <SidebarLabel>Tailwind Labs</SidebarLabel>
-                  <ChevronDownIcon />
-                </DropdownButton>
-                <DropdownMenu
-                  className="min-w-80 lg:min-w-64"
-                  anchor="bottom start"
-                >
-                  <DropdownItem href="/teams/1/settings">
-                    <Cog8ToothIcon />
-                    <DropdownLabel>Settings</DropdownLabel>
-                  </DropdownItem>
-                  <DropdownDivider />
-                  <DropdownItem href="/teams/1">
-                    <Avatar slot="icon" src="/tailwind-logo.svg" />
-                    <DropdownLabel>Tailwind Labs</DropdownLabel>
-                  </DropdownItem>
-                  <DropdownItem href="/teams/2">
-                    <Avatar
-                      slot="icon"
-                      initials="WC"
-                      className="bg-purple-500 text-white"
-                    />
-                    <DropdownLabel>Workcation</DropdownLabel>
-                  </DropdownItem>
-                  <DropdownDivider />
-                  <DropdownItem href="/teams/create">
-                    <PlusIcon />
-                    <DropdownLabel>New team&hellip;</DropdownLabel>
-                  </DropdownItem>
-                </DropdownMenu>
-              </Dropdown>
-              <SidebarSection className="max-lg:hidden">
-                <SidebarItem href="/search">
-                  <MagnifyingGlassIcon />
-                  <SidebarLabel>Search</SidebarLabel>
-                </SidebarItem>
-                <SidebarItem href="/inbox">
-                  <InboxIcon />
-                  <SidebarLabel>Inbox</SidebarLabel>
-                </SidebarItem>
-              </SidebarSection>
-            </SidebarHeader>
-            <SidebarBody>
-              <SidebarSection>
-                <SidebarItem href="/dashboard">
-                  <HomeIcon />
-                  <SidebarLabel>Home</SidebarLabel>
-                </SidebarItem>
-                <SidebarItem href="dashboard/ipo-calendar">
-                  <CalendarDaysIcon />
-                  <SidebarLabel>IPO Calendar</SidebarLabel>
-                </SidebarItem>
-                <SidebarItem href="/orders">
-                  <TicketIcon />
-                  <SidebarLabel>Orders</SidebarLabel>
-                </SidebarItem>
-                <SidebarItem href="/settings">
-                  <Cog6ToothIcon />
-                  <SidebarLabel>Settings</SidebarLabel>
-                </SidebarItem>
-                <SidebarItem href="/broadcasts">
-                  <MegaphoneIcon />
-                  <SidebarLabel>Broadcasts</SidebarLabel>
-                </SidebarItem>
-              </SidebarSection>
-              <SidebarSection className="max-lg:hidden">
-                <SidebarHeading>Upcoming Events</SidebarHeading>
-                <SidebarItem href="/events/1">
-                  Bear Hug: Live in Concert
-                </SidebarItem>
-                <SidebarItem href="/events/2">Viking People</SidebarItem>
-                <SidebarItem href="/events/3">Six Fingers — DJ Set</SidebarItem>
-                <SidebarItem href="/events/4">We All Look The Same</SidebarItem>
-              </SidebarSection>
-              <SidebarSpacer />
-              <SidebarSection>
-                <SidebarItem href="/support">
-                  <QuestionMarkCircleIcon />
-                  <SidebarLabel>Support</SidebarLabel>
-                </SidebarItem>
-                <SidebarItem href="/changelog">
-                  <SparklesIcon />
-                  <SidebarLabel>Changelog</SidebarLabel>
-                </SidebarItem>
-              </SidebarSection>
-            </SidebarBody>
-            <SidebarFooter className="max-lg:hidden">
-              <Dropdown>
-                <DropdownButton as={SidebarItem}>
-                  <span className="flex min-w-0 items-center gap-3">
-                    <Avatar
-                      src="https://catalyst.tailwindui.com/profile-photo.jpg"
-                      className="size-10"
-                      square
-                      alt=""
-                    />
-                    <span className="min-w-0">
-                      <span className="block truncate text-sm/5 font-medium text-zinc-950 dark:text-white">
-                        {user?.name && user.name}
-                      </span>
-                      <span className="block truncate text-xs/5 font-normal text-zinc-500 dark:text-zinc-400">
-                        {user?.email && user.email}
-                      </span>
-                    </span>
-                  </span>
-                  <ChevronUpIcon />
-                </DropdownButton>
-                <DropdownMenu
-                  className="min-w-64 bg-white lg:bg-zinc-100 dark:bg-zinc-900 dark:lg:bg-zinc-950 border border-zinc-950/10 pb-6 dark:border-white/10"
-                  anchor="top start"
-                >
-                  <DropdownItem href="/my-profile">
-                    <UserIcon />
-                    <DropdownLabel>My profile</DropdownLabel>
-                  </DropdownItem>
-                  <DropdownItem href="/settings">
-                    <Cog8ToothIcon />
-                    <DropdownLabel>Settings</DropdownLabel>
-                  </DropdownItem>
-                  <DropdownDivider />
-                  <DropdownItem href="/privacy-policy">
-                    <ShieldCheckIcon />
-                    <DropdownLabel>Privacy policy</DropdownLabel>
-                  </DropdownItem>
-                  <DropdownItem href="/share-feedback">
-                    <LightBulbIcon />
-                    <DropdownLabel>Share feedback</DropdownLabel>
-                  </DropdownItem>
-                  <DropdownDivider />
-                  <DropdownItem href="/logout">
-                    <ArrowRightStartOnRectangleIcon />
-                    <DropdownLabel>Sign out</DropdownLabel>
-                  </DropdownItem>
-                </DropdownMenu>
-              </Dropdown>
-            </SidebarFooter>
-          </Sidebar>
-        </aside>
-      }
+      sidebar={<DashboardSidebar user={user} />}
     >
       {children}
     </SidebarLayout>
